fdbd2cc9cdc84bb62d527ab5d8e0414415e86478
jtwray <tuckerwray84@gmail.com>
2019-05-11 21:26:52 -0400

    adds 'whispering-temple-56972.herokuapp.com' to 'allowed_hosts'

--- pioneerWeatherApp/settings.py
+++ pioneerWeatherApp/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-ALLOWED_HOSTS = [ '127.0.0.1','localhost']
+ALLOWED_HOSTS = [ 'whispering-temple-56972.herokuapp.com','127.0.0.1','localhost']
 
 LOGIN_REDIRECT_URL  = '/'
 # Application definition
